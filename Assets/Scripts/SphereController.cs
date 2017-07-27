e45a0485a221d37061366f10210e171641b7ce02
TimmPure <T.j.m.voskuil@gmail.com>
2017-07-27 18:14:22 +0200

    Cleaned up SphereController

--- Assets/Scripts/SphereController.cs
+++ Assets/Scripts/SphereController.cs
@@ -2,29 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent (typeof(Rigidbody2D))]
 public class SphereController : MonoBehaviour {
 
-    public static bool gameStarted;
+    public static bool gameStarted;                     //TODO: move this bool to a GameManager object
 
-    private float sphereSize = 2;
     private Rigidbody2D rb;
+    private float sphereSize = 2;
     private float minX, minY, maxX, maxY;
 
     private void Start()
     {
-        gameStarted = false;
         rb = GetComponent<Rigidbody2D>();
+        gameStarted = false;
         rb.simulated = false;
         minX = 2;
         maxX = 20;
         minY = 2;
         maxY = 20;
-
     }
 
     private void Update()
     {
-        if (!gameStarted && Input.GetKeyDown(KeyCode.Space))
+        if (!gameStarted && Input.GetKeyDown(KeyCode.Space))        //TODO: make game starting an event in GameManager, subscribing to it from here and UI?
         {
             rb.simulated = true;
             gameStarted = true;
@@ -36,17 +36,21 @@ public class SphereController : MonoBehaviour {
         if (coll.gameObject.CompareTag("Ground") && transform.localScale.y > 2)
         {
             Debug.Log("Sphere size decreased");
-            sphereSize = sphereSize - 3;
-            transform.localScale = new Vector3(Mathf.Clamp(sphereSize, minX, maxX), Mathf.Clamp(sphereSize, minY, maxY), transform.localScale.z);
+            sphereSize -= 3;
+            scaleSphere();
         }
     }
 
     private void OnTriggerEnter2D(Collider2D coll) {
         if (coll.gameObject.CompareTag("Powerup")) {
             Debug.Log("Nomnomnom");
-            sphereSize = sphereSize + 1;
-            transform.localScale = new Vector3(Mathf.Clamp(sphereSize, minX, maxX), Mathf.Clamp(sphereSize, minY, maxY), transform.localScale.z);
+            sphereSize += 1;
+            scaleSphere();
         }
+    }
+
+    private void scaleSphere() {
+        transform.localScale = new Vector3(Mathf.Clamp(sphereSize, minX, maxX), Mathf.Clamp(sphereSize, minY, maxY), transform.localScale.z);
     }
 
 
